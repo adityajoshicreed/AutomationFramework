7ff4ea760797da378563c1ec1ec006497eb56e64
Aditya Joshi <adi.joshi194@gmail.com>
2019-04-30 15:10:58 +0530

    Changed Chrome to headless

--- src/main/java/driver/DriverDefine.java
+++ src/main/java/driver/DriverDefine.java
@@ -23,7 +23,7 @@ public class DriverDefine {
 	public WebDriver getChrome() {
 		System.setProperty("webdriver.chrome.driver", Constants.chromePath);
 		ChromeOptions opt = new ChromeOptions();
-//		opt.addArguments("--headless");
+		opt.addArguments("--headless");
 		opt.addArguments("--no-sandbox");
 		opt.addArguments("--disable-dev-shm-usage");
 		driver = new ChromeDriver(opt);
